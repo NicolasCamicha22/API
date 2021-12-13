f2911801016da336feefa0a4e1dd596f328f1be0
NicolasCamicha22 <nicolascamicha22@gmail.com>
2021-12-13 11:57:28 -0300

    .

---
+++ src/config/locales/index.js
@@ -0,0 +1,10 @@
+const locales = [
+  {
+    locale: 'es',
+    messages: import('./es'),
+    //loadData: import(`@formatjs/intl-relativetimeformat/dist/locale-data/es`),
+  },
+]
+
+export default locales
+ 
